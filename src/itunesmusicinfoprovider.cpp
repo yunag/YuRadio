999aaaa0d4811c7afd397b42cbe991cdbf1a6115
Yunag <babushkin.ruslans@gmail.com>
2024-07-24 17:27:25 +0300

    feat(itunesmusicinfoprovider): add levenshtein function

--- src/itunesmusicinfoprovider.cpp
+++ src/itunesmusicinfoprovider.cpp
@@ -30,6 +30,47 @@ void ItunesMusicInfoProvider::provide(const QString &searchString) {
   }).onFailed([this](const NetworkError & /*err*/) { setState(Failed); });
 }
 
+qsizetype levenshteinDistance(const QString &source, const QString &target) {
+  if (source == target) {
+    return 0;
+  }
+
+  const qsizetype sourceCount = source.size();
+  const qsizetype targetCount = target.size();
+
+  if (source.isEmpty()) {
+    return targetCount;
+  }
+
+  if (target.isEmpty()) {
+    return sourceCount;
+  }
+
+  if (sourceCount > targetCount) {
+    return levenshteinDistance(target, source);
+  }
+
+  QList<int> column;
+  column.fill(0, targetCount + 1);
+  QList<int> previousColumn;
+  previousColumn.reserve(targetCount + 1);
+  for (int i = 0; i < targetCount + 1; i++) {
+    previousColumn.append(i);
+  }
+
+  for (int i = 0; i < sourceCount; i++) {
+    column[0] = i + 1;
+    for (int j = 0; j < targetCount; j++) {
+      column[j + 1] =
+        std::min({1 + column.at(j), 1 + previousColumn.at(1 + j),
+                  previousColumn.at(j) + (source.at(i) != target.at(j))});
+    }
+    column.swap(previousColumn);
+  }
+
+  return previousColumn.at(targetCount);
+}
+
 void ItunesMusicInfoProvider::handleReplyData(const QByteArray &data) {
   auto document = json::byteArrayToJson(data);
   if (!document) {
