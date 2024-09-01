e58325cb64ca0f1359cea6613d80e2262b827e3e
Yunag <babushkin.ruslans@gmail.com>
2024-09-01 19:23:00 +0300

    chore(versioning): use git versioning

--- cmake/VersionConfig.cmake
+++ cmake/VersionConfig.cmake
@@ -1,35 +1,35 @@
-# OBS CMake common version helper module
+# NOTE: File were taken from obs-studio repo
 
 include_guard(GLOBAL)
 
-set(_obs_version ${_obs_default_version})
-set(_obs_version_canonical ${_obs_default_version})
+set(_yuradio_version "1" "0" "0")
+set(_yuradio_version_canonical ${_yuradio_version})
 
-# Attempt to automatically discover expected OBS version
-if(NOT DEFINED OBS_VERSION_OVERRIDE AND EXISTS
-                                        "${CMAKE_CURRENT_SOURCE_DIR}/.git")
+# Attempt to automatically discover expected YURADIO version
+if(NOT DEFINED YURADIO_VERSION_OVERRIDE AND EXISTS
+                                            "${CMAKE_CURRENT_SOURCE_DIR}/.git")
   execute_process(
     COMMAND git describe --always --tags --dirty=-modified
-    OUTPUT_VARIABLE _obs_version
+    OUTPUT_VARIABLE _yuradio_version
     ERROR_VARIABLE _git_describe_err
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-    RESULT_VARIABLE _obs_version_result
+    RESULT_VARIABLE _yuradio_version_result
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   if(_git_describe_err)
-    message(FATAL_ERROR "Could not fetch OBS version tag from git.\n"
+    message(FATAL_ERROR "Could not fetch YURADIO version tag from git.\n"
                         ${_git_describe_err})
   endif()
 
-  if(_obs_version_result EQUAL 0)
+  if(_yuradio_version_result EQUAL 0)
     string(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+).*" "\\1;\\2;\\3"
-                         _obs_version_canonical ${_obs_version})
+                         _yuradio_version_canonical ${_yuradio_version})
   endif()
-elseif(DEFINED OBS_VERSION_OVERRIDE)
-  if(OBS_VERSION_OVERRIDE MATCHES "([0-9]+)\\.([0-9]+)\\.([0-9]+).*")
+elseif(DEFINED YURADIO_VERSION_OVERRIDE)
+  if(YURADIO_VERSION_OVERRIDE MATCHES "([0-9]+)\\.([0-9]+)\\.([0-9]+).*")
     string(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+).*" "\\1;\\2;\\3"
-                         _obs_version_canonical ${OBS_VERSION_OVERRIDE})
-    set(_obs_version ${OBS_VERSION_OVERRIDE})
+                         _yuradio_version_canonical ${YURADIO_VERSION_OVERRIDE})
+    set(_yuradio_version ${YURADIO_VERSION_OVERRIDE})
   else()
     message(
       FATAL_ERROR
@@ -39,47 +39,43 @@ elseif(DEFINED OBS_VERSION_OVERRIDE)
 endif()
 
 # Set beta/rc versions if suffix included in version string
-if(_obs_version MATCHES "[0-9]+\\.[0-9]+\\.[0-9]+-rc[0-9]+")
+if(_yuradio_version MATCHES "[0-9]+\\.[0-9]+\\.[0-9]+-rc[0-9]+")
   string(REGEX REPLACE "[0-9]+\\.[0-9]+\\.[0-9]+-rc([0-9]+).*$" "\\1"
-                       _obs_release_candidate ${_obs_version})
-elseif(_obs_version MATCHES "[0-9]+\\.[0-9]+\\.[0-9]+-beta[0-9]+")
+                       _yuradio_release_candidate ${_yuradio_version})
+elseif(_yuradio_version MATCHES "[0-9]+\\.[0-9]+\\.[0-9]+-beta[0-9]+")
   string(REGEX REPLACE "[0-9]+\\.[0-9]+\\.[0-9]+-beta([0-9]+).*$" "\\1"
-                       _obs_beta ${_obs_version})
+                       _yuradio_beta ${_yuradio_version})
 endif()
 
-list(GET _obs_version_canonical 0 OBS_VERSION_MAJOR)
-list(GET _obs_version_canonical 1 OBS_VERSION_MINOR)
-list(GET _obs_version_canonical 2 OBS_VERSION_PATCH)
+list(GET _yuradio_version_canonical 0 YURADIO_VERSION_MAJOR)
+list(GET _yuradio_version_canonical 1 YURADIO_VERSION_MINOR)
+list(GET _yuradio_version_canonical 2 YURADIO_VERSION_PATCH)
 
-set(OBS_RELEASE_CANDIDATE ${_obs_release_candidate})
-set(OBS_BETA ${_obs_beta})
+set(YURADIO_RELEASE_CANDIDATE ${_yuradio_release_candidate})
+set(YURADIO_BETA ${_yuradio_beta})
 
-message("MAJOR ${OBS_VERSION_MAJOR}")
-message("MINOR ${OBS_VERSION_MINOR}")
-message("PATCH ${OBS_VERSION_PATCH}")
+string(REPLACE ";" "." YURADIO_VERSION_CANONICAL
+               "${_yuradio_version_canonical}")
+string(REPLACE ";" "." YURADIO_VERSION "${_yuradio_version}")
 
-string(REPLACE ";" "." OBS_VERSION_CANONICAL "${_obs_version_canonical}")
-string(REPLACE ";" "." OBS_VERSION "${_obs_version}")
-
-if(OBS_RELEASE_CANDIDATE GREATER 0)
+if(YURADIO_RELEASE_CANDIDATE GREATER 0)
   message(
     AUTHOR_WARNING
       "******************************************************************************\n"
-      "  + OBS-Studio - Release candidate detected, OBS_VERSION is now: ${OBS_VERSION}\n"
+      "  + YURADIO-Studio - Release candidate detected, YURADIO_VERSION is now: ${YURADIO_VERSION}\n"
       "******************************************************************************"
   )
-elseif(OBS_BETA GREATER 0)
+elseif(YURADIO_BETA GREATER 0)
   message(
     AUTHOR_WARNING
       "******************************************************************************\n"
-      "  + OBS-Studio - Beta detected, OBS_VERSION is now: ${OBS_VERSION}\n"
+      "  + YURADIO-Studio - Beta detected, YURADIO_VERSION is now: ${YURADIO_VERSION}\n"
       "******************************************************************************"
   )
 endif()
 
-unset(_obs_default_version)
-unset(_obs_version)
-unset(_obs_version_canonical)
-unset(_obs_release_candidate)
-unset(_obs_beta)
-unset(_obs_version_result)
+unset(_yuradio_version)
+unset(_yuradio_version_canonical)
+unset(_yuradio_release_candidate)
+unset(_yuradio_beta)
+unset(_yuradio_version_result)
