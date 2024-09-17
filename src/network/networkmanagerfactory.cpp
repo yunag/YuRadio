6278daae42a5176b7e635264210c15a513d974ca
Yunag <babushkin.ruslans@gmail.com>
2024-09-17 21:42:18 +0300

    fix(networkmanagerfactory): set correct cache location

--- src/network/networkmanagerfactory.cpp
+++ src/network/networkmanagerfactory.cpp
@@ -1,3 +1,5 @@
+#include <QStandardPaths>
+
 #include "networkmanagerfactory.h"
 
 #include "networkmanager.h"
@@ -9,7 +11,8 @@ QNetworkAccessManager *NetworkManagerFactory::create(QObject *parent) {
   auto *networkAccessManager = new NetworkManager(parent);
   auto *diskCache = new QNetworkDiskCache(parent);
 
-  diskCache->setCacheDirectory("requestCache");
+  diskCache->setCacheDirectory(
+    QStandardPaths::writableLocation(QStandardPaths::CacheLocation));
   networkAccessManager->setCache(diskCache);
 
   return networkAccessManager;
