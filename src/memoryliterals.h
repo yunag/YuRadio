50c2b8134b6a410a94bf90872d208f70187b39bb
Yunag <babushkin.ruslans@gmail.com>
2024-10-24 15:44:19 +0300

    chore(memoryliterals): don't use suffix

--- src/memoryliterals.h
+++ src/memoryliterals.h
@@ -2,23 +2,23 @@
 
 namespace MemoryLiterals {
 
-constexpr std::size_t operator""_KiB(unsigned long long int x) {
-  return 1024ULL * x;
+constexpr std::size_t operator""_KiB(unsigned long long x) {
+  return 1024 * x;
 }
 
-constexpr std::size_t operator""_MiB(unsigned long long int x) {
+constexpr std::size_t operator""_MiB(unsigned long long x) {
   return 1024_KiB * x;
 }
 
-constexpr std::size_t operator""_GiB(unsigned long long int x) {
+constexpr std::size_t operator""_GiB(unsigned long long x) {
   return 1024_MiB * x;
 }
 
-constexpr std::size_t operator""_TiB(unsigned long long int x) {
+constexpr std::size_t operator""_TiB(unsigned long long x) {
   return 1024_GiB * x;
 }
 
-constexpr std::size_t operator""_PiB(unsigned long long int x) {
+constexpr std::size_t operator""_PiB(unsigned long long x) {
   return 1024_TiB * x;
 }
 
